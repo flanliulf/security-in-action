f165fbf906eee560ab3fe9bc743941da445fc25e
liufan <flanliulf@gmail.com>
2020-04-12 13:37:55 +0800

    guava 实现简单流控

---
+++ user-api/src/main/java/com/fancyliu/security/user/RatelimitFilter.java
@@ -0,0 +1,35 @@
+package com.fancyliu.security.user;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RatelimitFilter extends OncePerRequestFilter {
+
+    private RateLimiter rateLimiter = RateLimiter.create(1);
+
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (rateLimiter.tryAcquire()) {
+            filterChain.doFilter(request, response);
+        } else {
+            // 限流控制
+
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.getWriter().write("too many request");
+            response.getWriter().flush();
+
+            return;
+        }
+
+    }
+}
